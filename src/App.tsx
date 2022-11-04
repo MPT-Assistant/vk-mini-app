96854e674835e275ee34807bf90032729687339e
rus_anonym <alexandrsemin2033@gmail.com>
2022-11-04 20:26:02 +0300

    add hooks for VK bridge

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import {
   AdaptivityProvider,
   AppRoot,
   ConfigProvider,
+  platform,
+  Platform,
   WebviewType,
 } from "@vkontakte/vkui";
 import Layout from "./Layout";
@@ -12,13 +14,54 @@ import "./CSS/index.css";
 import "@vkontakte/vkui/dist/vkui.css";
 import "@vkontakte/vkui/dist/unstable.css";
 import "@vkontakte/vkui/dist/fonts.css";
-import { AppearanceType } from "@vkontakte/vk-bridge";
+import bridge, {
+  AppearanceType,
+  VKUpdateConfigData,
+} from "@vkontakte/vk-bridge";
+import { SMALL_TABLET_SIZE } from "@vkontakte/vkui/dist/components/AdaptivityProvider/AdaptivityProvider";
+
+function currentPlatform(): Platform {
+  if (
+    window.innerWidth >= SMALL_TABLET_SIZE &&
+    window.matchMedia("(orientation: landscape)").matches
+  ) {
+    return Platform.VKCOM;
+  }
+
+  return platform() as Platform;
+}
 
 const App = () => {
+  const [platform, setPlatform] = useState<Platform>(currentPlatform());
   const [appearance, setAppearance] = useState<AppearanceType>("dark");
+
+  useEffect((): void => {
+    function updateConfig({ appearance }: VKUpdateConfigData) {
+      if (appearance) setAppearance(appearance);
+    }
+
+    bridge.send("VKWebAppGetConfig").then((config) => {
+      updateConfig(config as VKUpdateConfigData);
+    });
+    bridge.subscribe(async ({ detail: { type, data } }) => {
+      if (type === "VKWebAppUpdateConfig") {
+        updateConfig(data as VKUpdateConfigData);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    function onResize(): void {
+      setPlatform(currentPlatform());
+    }
+
+    window.addEventListener("resize", onResize, false);
+    return () => window.removeEventListener("resize", onResize, false);
+  }, []);
 
   return (
     <ConfigProvider
+      platform={platform}
       appearance={appearance}
       webviewType={WebviewType.INTERNAL}
       transitionMotionEnabled={false}
