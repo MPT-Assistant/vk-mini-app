15d9b0742f05e483aadbcb65b7c4202b2e38095a
rus_anonym <alexandrsemin2033@gmail.com>
2022-11-05 03:37:25 +0300

    add Replacements page

---
+++ src/pages/Replacements/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Group, Placeholder } from "@vkontakte/vkui";
+
+const Replacements = () => {
+  return (
+    <Group>
+      <Placeholder title="There will be something here soon..." />
+    </Group>
+  );
+};
+
+export default Replacements;
